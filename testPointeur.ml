bcbd2e65aa43ace73aaea4477517df2b9e1ced8f
pixtop <pixtopmc@gmail.com>
2018-12-26 17:20:52 +0100

    Fix pointer test

--- testPointeur.ml
+++ testPointeur.ml
@@ -5,60 +5,58 @@ open Type
 
 exception ErreurNonDetectee;;
 
-let path = "../../fichiersRat/src-rat-pointeur-test/"
-
-let%test_unit "test"=
-  let _ = compiler path^"test.rat" in ()
+let%test_unit "test" =
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/test.rat" in ()
 
 let%test_unit "test2" =
-  let _ = compiler path^"test2.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/test2.rat" in ()
 
 let%test_unit "testAffectation1" =
-  let _ = compiler path^"testAffectation1.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation1.rat" in ()
 
 let%test_unit "testAffectation2" =
   try
-    let _ = compiler path^"testAffectation2.rat"
+    let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation2.rat"
       in raise ErreurNonDetectee
   with
   | TypeInattendu(Rat, Int) -> ()
 
 let%test_unit "testAffectation3" =
-  let _ = compiler path^"testAffectation3.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation3.rat" in ()
 
 let%test_unit "testAffectation4" =
   try
-    let _ = compiler path^"testAffectation4.rat"
+    let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation4.rat"
       in raise ErreurNonDetectee
   with
   | TypeInattendu(Rat, Pt(Rat)) -> ()
 
 let%test_unit "testAffectation5" =
-  let _ = compiler path^"testAffectation5.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation5.rat" in ()
 
 let%test_unit "testAffectation6" =
   try
-    let _ = compiler path^"testAffectation6.rat"
+    let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation6.rat"
       in raise ErreurNonDetectee
   with
   | TypeInattendu(Pt(Int), Pt(Rat)) -> ()
 
 let%test_unit "testAffectation7" =
-  let _ = compiler path^"testAffectation7.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation7.rat" in ()
 
 let%test_unit "testAffectation8" =
-  let _ = compiler path^"testAffectation8.rat" in ()
+  let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation8.rat" in ()
 
 let%test_unit "testAffectation9" =
   try
-    let _ = compiler path^"testAffectation9.rat"
+    let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation9.rat"
       in raise ErreurNonDetectee
   with
   | PasUnPointeur -> ()
 
 let%test_unit "testAffectation10" =
   try
-    let _ = compiler path^"testAffectation10.rat"
+    let _ = compiler "../../fichiersRat/src-rat-pointeur-test/testAffectation10.rat"
     in raise ErreurNonDetectee
   with
   | TypeInattendu(Undefined, Int) -> ()
