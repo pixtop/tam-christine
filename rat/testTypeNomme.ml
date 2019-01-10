eea1898e23b9e354a37ffb1af4bdb42be0532fc9
pixtop <pixtopmc@gmail.com>
2019-01-10 12:51:11 +0100

    Added more test to named typ

--- rat/testTypeNomme.ml
+++ rat/testTypeNomme.ml
@@ -1,7 +1,37 @@
 open Compilateur
-(* open Exceptions *)
+open Exceptions
+open TestTam
 
-(* exception ErreurNonDetectee *)
+
+exception ErreurNonDetectee
 
 let%test_unit "test"=
   let _ = compiler "../../../fichiersRat/src-rat-typenomme-test/test.rat" in ()
+
+let%test_unit "test2"=
+  let _ = compiler "../../../fichiersRat/src-rat-typenomme-test/test2.rat" in ()
+
+let%expect_test "test3"=
+  runtam "../../../fichiersRat/src-rat-typenomme-test/test3.rat";
+  [%expect{| 1 |}]
+
+let%test_unit "testDeclaration"=
+  try
+    let _ = compiler "../../../fichiersRat/src-rat-typenomme-test/testDeclaration.rat"
+    in raise ErreurNonDetectee
+  with
+  | DoubleDeclaration ("Int2") -> ()
+
+let%test_unit "testDeclaration2"=
+  try
+    let _ = compiler "../../../fichiersRat/src-rat-typenomme-test/testDeclaration2.rat"
+    in raise ErreurNonDetectee
+  with
+  | IdentifiantNonDeclare ("Int") -> ()
+
+let%test_unit "testDeclaration3"=
+  try
+    let _ = compiler "../../../fichiersRat/src-rat-typenomme-test/testDeclaration3.rat"
+    in raise ErreurNonDetectee
+  with
+  | IdentifiantNonDeclare ("Couple") -> ()
